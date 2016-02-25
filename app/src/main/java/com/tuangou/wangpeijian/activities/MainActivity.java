3aca4d6306db7faa508e0b354384d63dbc476e0a
506605454 <506605454@qq.com>
2016-02-25 22:09:02 +0800

    package creade

--- app/src/main/java/com/tuangou/wangpeijian/activities/MainActivity.java
+++ app/src/main/java/com/tuangou/wangpeijian/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tuangou.wangpeijian;
+package com.tuangou.wangpeijian.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tuangou.wangpeijian.R;
 
 public class MainActivity extends AppCompatActivity {
 
